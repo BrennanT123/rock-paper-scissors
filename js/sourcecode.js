cfb589c5dc5dc5faed20d2761e312054fd4ade49
BrennanT123 <113391949+BrennanT123@users.noreply.github.com>
2024-10-24 14:59:13 -0400

    Add UI
    Adds UI in index.html.

--- js/sourcecode.js
+++ js/sourcecode.js
@@ -3,6 +3,18 @@ console.log("Hello World");
 //function
 // COMPUTE computer choice 
     //RETURN choice
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const userScore = document.querySelector('#userScore');
+const computerScore = document.querySelector('#computerScore');
+const endGame = document.querySelector('#endGame');
+
+rock.addEventListener('click',playRound);
+paper.addEventListener('click',playRound);
+scissors.addEventListener('click',playRound);
+
+
 
 function getComputerChoice()
 {
@@ -24,66 +36,35 @@ function getComputerChoice()
     return choice;
 }
 
-//function
-//OBTAIN user choice
-    //RETURN use choice
-
-function getUserChoice(){
-    choicenum = parseInt(prompt("Return 1 for rock, 2 for paper, and 3 for scissors",1));
-    switch(choicenum){
-        case 1:
-            choice = "Rock"
-            break;
-        case 2:
-            choice = "Paper";
-            break;
-        case 3:
-            choice = "Scissors";
-            break;
-        default:
-            choice = "Error returned";
-            break;
-        }
-        return choice;
-
-}
-var userscore = 0;
-var computerscore = 0;
+var tempUserScore = 0;
+var tempComputerScore = 0;
 //PLAY ROUND FUNCTION 
-function playround()
+function playRound()
 {
-userchoice = getUserChoice();
+userchoice = this.textContent;
+console.log(userchoice);
 computerchoice = getComputerChoice();
 //COMPARE user choice\
 //IF user wins THEN return win
 
 console.log(`User Chose `+ userchoice +` Computer chose `+computerchoice);
 if(userchoice === computerchoice){
-    console.log("THe game was a tie!");
+    console.log("The game was a tie!");
 }else if((userchoice === `Rock` && computerchoice === `Scissors`)||(userchoice==='Scissors' && computerchoice===`Paper`)||
 (userchoice==='Paper'&&computerchoice==='Rock')){
     console.log("User wins!");
-    userscore++;
+    tempUserScore++;
 }else if((computerchoice === `Rock` && userchoice === `Scissors`)||(computerchoice==='Scissors' && userchoice===`Paper`)||
 (computerchoice==='Paper'&&userchoice==='Rock')){
     console.log("Computer wins!");
-    computerscore++;
+    tempComputerScore++;
 }else{
     console.log("Error occured");
 }
+userScore.textContent = `User Score: ` + tempUserScore;
+computerScore.textContent = `Computer Score: `+ tempComputerScore;
 
-
-
+    
 }
 
-//Playgame fucntion
-function playgame()
-{
-    playround();
-    playround();
-    playround();
-    playround();
-    playround();
-    console.log(`Final Score is... \nUSER: `+userscore+`\nCOMPUTER: `+computerscore)
-}
-playgame();
+
